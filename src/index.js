86b7806acded1252f4eb6f5d0759ec67b7d3e0ad
Dawid-Oskwarek <dawidoskwarek97@gmail.com>
2022-07-25 21:28:05 +0100

    setup

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import { expect } from 'chai';
+import { getLinks, checkLinks, excludeLinks } from './helper.js';
+
+async function run() {
+    try {
+        const urlsToCheck = process.env['urlsToCheck'].split(',');
+        const excludedUrls = cprocess.env['excludedUrls'].split(',');
+
+        const links = await getLinks(urlsToCheck);
+        const newLinks = excludeLinks(links, excludedUrls);
+        const check = await checkLinks(newLinks);
+        expect(check).to.equal(true);
+    }
+    catch (error) {
+        throw new Error(error);
+    }
+}
