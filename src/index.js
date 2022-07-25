f9d72d056138f74475cd78fad63968f5fbc5ed6e
Dawid-Oskwarek <dawidoskwarek97@gmail.com>
2022-07-25 21:42:30 +0100

    test

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import { getLinks, checkLinks, excludeLinks } from './helper.js';
 
 async function run() {
     try {
-        const urlsToCheck = (process.env['urlsToCheck']).split(',');
-        const excludedUrls = (process.env['excludedUrls']).split(',');
+        const urlsToCheck = process.env.urlsToCheck.split(',');
+        const excludedUrls = process.env.excludedUrls.split(',');
 
         const links = await getLinks(urlsToCheck);
         const newLinks = excludeLinks(links, excludedUrls);
